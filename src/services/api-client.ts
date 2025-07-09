6fbef1b7bc8a6227acc399ae5caf1872cb01f4fb
fateme abbasazadeh <fabbaszadeh77@yahoo.com>
2025-07-09 20:55:06 +0330

    Refactor: Extract a query object

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -10,52 +10,6 @@ import { Platform } from "../hooks/useGames";
 //     }
 // })
 
-// const apiClient: AxiosInstance = axios.create({
-//   baseURL: "https://api.rawg.io/api",
-//   params: { key: "dummy-key" },
-// });
-
-// if (import.meta.env.MODE === "development") {
-//   const mock = new MockAdapter(apiClient, { delayResponse: 300 });
-
-//   // استخراج پلتفرم‌ها به صورت یکتا از همه بازی‌ها
-//   const platformsSet = new Map<number, Platform>();
-
-//   mockData.games.forEach((game) => {
-//     game.platforms.forEach((p) => {
-//       const platform = p.platform;
-//       if (!platformsSet.has(platform.id)) {
-//         platformsSet.set(platform.id, platform);
-//       }
-//     });
-//   });
-
-//   const platforms = Array.from(platformsSet.values());
-
-//   mock.onGet("/genres").reply(200, { results: mockData.genres });
-
-//   mock.onGet("/platforms/lists/parents").reply(200, { results: platforms });
-
-//   mock.onGet("/games").reply((config) => {
-//     const genreId = Number(config.params?.genres);
-
-//     if (!genreId) {
-//       return [200, { results: mockData.games }];
-//     }
-
-//     const filteredGames = mockData.games.filter((game) =>
-//       game.genres.some((g) => g.id === genreId)
-//     );
-
-//     return [200, { results: filteredGames }];
-//   });
-// }
-
-// export default apiClient;
-
-
-
-
 const apiClient: AxiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: { key: import.meta.env.VITE_RAWG_KEY ?? "dummy-key" },
@@ -71,8 +25,8 @@ if (import.meta.env.MODE === "development") {
   const platformMap = new Map<number, Platform>();
   mockData.games.forEach((game) =>
     game.platforms.forEach(({ platform }) =>
-      platformMap.set(platform.id, platform),
-    ),
+      platformMap.set(platform.id, platform)
+    )
   );
   const platforms = Array.from(platformMap.values());
 
@@ -98,12 +52,12 @@ if (import.meta.env.MODE === "development") {
 
     if (genreId)
       results = results.filter((g) =>
-        g.genres.some((gen) => gen.id === genreId),
+        g.genres.some((gen) => gen.id === genreId)
       );
 
     if (platformIds.length)
       results = results.filter((g) =>
-        g.platforms.some((p) => platformIds.includes(p.platform.id)),
+        g.platforms.some((p) => platformIds.includes(p.platform.id))
       );
 
     return [200, { results }];
